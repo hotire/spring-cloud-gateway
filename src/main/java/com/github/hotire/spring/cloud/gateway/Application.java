ecf33dc0a39990ee7fd85579ad38bb45a343116b
hotire <gngh0101@gmail.com>
2020-06-13 11:52:06 +0900

    Rename Application

--- src/main/java/com/github/hotire/spring/cloud/gateway/Application.java
+++ src/main/java/com/github/hotire/spring/cloud/gateway/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudGatewayApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
